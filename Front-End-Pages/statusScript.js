5672e991cd150371b70548a0093f51f6a24e7869
psatid <psatid32@gmail.com>
2019-02-13 00:05:03 +0700

    Timer for current status page

---
+++ Front-End-Pages/statusScript.js
@@ -0,0 +1,65 @@
+//Create Timer
+function Stopwatch(elem) {
+    var time = 0;
+    var offset;
+    var interval;
+
+    function update() {
+        time += delta();
+        elem.textContent = timeFormatter(time);
+    }
+
+    function delta() {
+      var now = Date.now();
+      var timePassed = now - offset;
+
+      offset = now;
+
+      return timePassed;
+    }
+
+    function timeFormatter(time) {
+      time = new Date(time);
+
+      var minutes = time.getMinutes().toString();
+      var seconds = time.getSeconds().toString();
+      var intHours = 7 - time.getHours();
+      var hours = intHours.toString();
+
+      if (minutes.length < 2) {
+        minutes = '0' + minutes;
+      }
+
+      if (seconds.length < 2) {
+        seconds = '0' + seconds;
+      }
+
+      if (hours.length < 2) {
+        hours = '0' + hours;
+      }
+
+      return hours + ' : ' + minutes + ' : ' + seconds;
+    }
+
+    this.start = function() {
+      interval = setInterval(update.bind(this), 10);
+      offset = Date.now();
+    };
+
+    this.stop = function() {
+      clearInterval(interval);
+      interval = null;
+    };
+
+    this.reset = function() {
+      time = 0;
+      update();
+    };
+
+}
+
+//Initiate Timer
+var timer = document.getElementById("timer");
+var watch = new Stopwatch(timer);
+
+watch.start();
