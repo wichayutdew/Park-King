57a8d455524fa21c909e6a267204dc5fc53ddac1
wichayutdew <dew10170@hotmail.com>
2019-02-16 17:14:52 +0700

    link home menu bar

--- Front-End-Pages/homeScript.js
+++ Front-End-Pages/homeScript.js
@@ -9,3 +9,19 @@ document.getElementById("myQRBut").addEventListener("click", function() {
 document.getElementById("reserveBut").addEventListener("click", function() {
   window.location = 'reserve.html';
 });
+
+document.getElementById("homeMenu").addEventListener("click", function() {
+  window.location = 'home.html';
+});
+
+document.getElementById("reserveMenu").addEventListener("click", function() {
+  window.location = 'reserve.html';
+});
+
+document.getElementById("statusMenu").addEventListener("click", function() {
+  window.location = 'status.html';
+});
+
+document.getElementById("qrMenu").addEventListener("click", function() {
+  window.location = 'showQR.html';
+});
