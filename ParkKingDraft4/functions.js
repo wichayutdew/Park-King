f98eb2ed04d43ab445b8b27404bc304d073f32e5
Wichayut Phongphanpanya <wichayutsmacbookpro@Wichayuts-MacBook-Pro.local>
2019-03-20 09:09:37 +0700

    new function

--- ParkKingDraft4/functions.js
+++ ParkKingDraft4/functions.js
@@ -1,18 +1,45 @@
-function checkAvailabiliy(){
-  //
+function checkAvailabiliy(buildingname){
+  var request = new request("SELECT P.Floor, P.Spot FROM dbo.ParkingSpot P WHERE P.BuildingName = ' " + buildingname + " ' AND P.isfull = 0 ORDER BY Floor ASC, Spot ASC",
+    function(err, rowCount, rows) {
+      if (err) {
+        done(err);
+      } else {
+
+      }
+    });
+  var returnedValue = {};
+  request.on('row', function(columns) {
+    columns.forEach(function(column) {
+      returnedValue.push(column.value);
+    });
+    //console.log(returnedValue);
+  });
+  request.on('requestCompleted', function() {
+    //connection.close();
+    //error here
+  });
+  connection.execSql(request);
+  return returnedValue[0];
 }
 
-function cancelFrequency(){
-
+function cancelFrequency(username){
+  var cancel = getterSetter.getUserCancel(username);
+  return cancel;
 }
 
-function timer(){
+function timer(time){
 
 }
+function getTime(){
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  return time;
+}
 
-function generateReserveID(){
-
-  return reserveID;
+function generateTokenID(){
+  const uuidv4 = require('uuid/v4');
+  var tokenID = uuidv4();
+  return tokenID;
 }
 
 function generateQR(){
@@ -27,13 +54,17 @@ function closeFlap(spot){
 //motor
 }
 
-function isQREqual(qr1,qr2){
-
+function isQREqual(QRcode,tokenID){
+  if(QRcode == tokenID){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function checkIn(reserveID){
-  isQREqual(reserve.getReserveID, qr2);
-  openFlap(reserve.getReserveID);
+  isQREqual(QRcode,getterSetter.getReserveID);
+  //openFlap(reserve.getReserveID);
   timer(30).start;
   if(isParked){
     closeFlap(spot);
@@ -79,5 +110,5 @@ function checkIfCarLeft(spot);{
 }
 
 function makeReceipt(reserveID){
-  
+
 }
