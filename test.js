3e533bd4ab4a1a374bd8f49ccdb206ba6e28d6b7
Wichayut Phongphanpanya <wichayutsmacbookpro@Wichayuts-MacBook-Pro.local>
2019-03-20 13:17:17 +0700

    stopwatch and countdown timer

--- test.js
+++ test.js
@@ -1,20 +1,25 @@
 var Stopwatch = require('statman-stopwatch');
 var stopwatch = new Stopwatch();
-stopwatch.start();
-var x = setInterval(write,1000);
 
-function write() {
-  var input = parseInt(stopwatch.read()/1000);
-  var hr;
-  var min;
-  var sec;
-  if(input >= 3600){
-    if(input >= 60){
-      min += 1;
-      sec = 0;
-    }
-    hr += 1;
-    min = 0;
-  }
-  console.log(hr+":"+min+":"+sec);
+stopwatch.start();
+
+function elaspedTime() {
+  var time = parseInt(stopwatch.read()/1000);
+  var hours = ~~(time / 3600);
+  var min = ~~((time % 3600) / 60);
+  var sec = time % 60;
+  console.log(hours+":"+min+":"+sec);
+}
+writeTime();
+setInterval(writeTime,1000);
+
+
+function Timer(seconds) {
+  var myInterval =  setInterval(function () {
+        duration --;
+        //console.log(duration);
+        if(duration < 1){
+          clearInterval(myInterval);
+        }
+    }, 1000);
 }
