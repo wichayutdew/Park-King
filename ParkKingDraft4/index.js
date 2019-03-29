748ef04fef74c4a5c543249e703190c53819485c
Theetouch J <theetouchjareonwatanan@Theetouchs-MacBook-Pro.local>
2019-03-29 12:41:38 +0700

    new database - t

--- ParkKingDraft4/index.js
+++ ParkKingDraft4/index.js
@@ -8,37 +8,80 @@ const multer = require('multer');
 const autoReap  = require('multer-autoreap');
 var fs = require('fs');
 
+
 //tedious section
 var Connection = require('tedious').Connection;
+var ConnectionPool = require('tedious-connection-pool');
 var Request = require('tedious').Request;
 var TYPES =require('tedious').TYPES;
+
 //authentication section
 passport = require('passport');
 LocalStrategy = require('passport-local');
 
-//===================================================================================================================================================
 
+//===================================================================================================================================================
+var poolConfig = {
+    min: 0,
+    max: 64,
+    log: true,
+};
 //azure database connection config
 var config = {
     server: 'parking.database.windows.net',
-    options:{
+      //useColumnNames: true,
+    database: 'Park_King',
+    userName: 'thee',
+    password: 'OtoroLover420',
+    options: {
       encrypt: true,
       database: 'Park_King',
       rowCollectionOnRequestCompletion: true,
-      rowCollectionOnDone: true,},
-      //useColumnNames: true,
-	authentication: {
-    type: 'default',
-    options: {
-      database: 'Park_King',
-      userName: 'thee',
-      password: 'OtoroLover420',
-
-    }
-  }
+      rowCollectionOnDone: true
+    },
+	  // authentication: {
+    //   type: 'azure-active-directory-password',
+    //   options: {
+    //     database: 'Park_King',
+    //     userName: 'Theetouch.J@student.chula.ac.th',
+    //     password: '7ac19pn8',
+    //
+    //   }
+    // }
   };
 
-var connection = new Connection(config);
+//var connection = new Connection(config);
+var pool = new ConnectionPool(poolConfig, config);
+
+pool.on('error', function(err) {
+    console.error(err);
+});
+
+// pool.acquire(function (err, connection) {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//
+//     //use the connection as normal
+//     var request = new Request('select 42', function(err, rowCount) {
+//         if (err) {
+//             console.error(err);
+//             return;
+//         }
+//
+//         console.log('rowCount: ' + rowCount);
+//
+//         //release the connection back to the pool when finished
+//         connection.release();
+//     });
+//
+//     request.on('row', function(columns) {
+//         console.log('value: ' + columns[0].value);
+//     });
+//
+//     connection.execSql(request);
+// });
 //===================================================================================================================================================
 
 //APP CONFIG
@@ -49,100 +92,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 //===================================================================================================================================================
 // Passport Module
 //===================================================================================================================================================
-    function deserializer(username,done){
-    var request = new Request(
-        "SELECT * FROM dbo.Customer WHERE Username = @username",
-        function(err,rows){
-            //done(err,rows[0]);
-        }
-    );
-    //set parameterized query
-    request.addParameter('username',TYPES.VarChar,username[0]);
-    var deserializing = [];
-    request.on('row', function (columns) {
-        columns.forEach(function(column) {
-            deserializing.push(column.value);
-        });
-        //console.log(deserializing);
-    });
 
-    request.on('requestCompleted', function (err,rows) {
-        // Next SQL statement.
-        done(err, deserializing);
-    });
 
-    connection.execSql(request);
-}
-    function _signup(req,username,password,done){
-    console.log('Sign-up requested')
-    //var IMG = base64_encode(req.body.profilePic);
-    var img = fs.readFileSync(req.file.path);
-    var encode_image = img.toString('base64');
-    //console.log(encode_image);
-
-    // var finalImg = {
-    //      contentType: req.file.mimetype,
-    //      image:  new Buffer(encode_image, 'base64')
-    // };
-    var customer_info = {
-      username :req.body.username,
-      password : req.body.password,
-      passwordCheck : req.body.passwordConfirmation,
-      email : req.body.email,
-      fname : req.body.firstName,
-      lname : req.body.lastName,
-      occupation: req.body.occupation,
-      studentID: req.body.studentID,
-      professorID: req.body.professorID,
-      guestID: req.body.NationalID,
-      CustomerPicture: encode_image,
-      Reserveable: 1,
-    };
-    var request = new Request(
-        "SELECT * FROM dbo.Customer WHERE Username = @username",
-        function (err, rowCount, rows){
-            //console.log(rows);
-            //console.log("above row object");
-            if (err){
-                console.log("signup error");
-                return done(err);
-            }
-            if (customer_info.password!=customer_info.passwordCheck){
-                console.log('password does not match');
-                return done(null,false);
-            }
-            if (rows.length != 0) {
-                console.log('this email is already taken');
-                return done(null, false);
-            }else {
-
-                console.log('this email doesnot taken')
-                // if there is no user with that email
-                // create the use
-                var newUserMysql = new Object();
-
-                newUserMysql.username = username;
-                newUserMysql.password = password; // use the generateHash function in our user model
-
-                newUserMysql.id = rows.insertId;
-
-                insert_newCustomer(customer_info,done)
-                return done(null, newUserMysql);
-            }
-        }
-    );
-    //set parameterized query
-    request.addParameter('username',TYPES.VarChar,customer_info.username);
-    request.addParameter('password',TYPES.VarChar,customer_info.password);
-
-    request.on('requestCompleted', function () {
-
-    });
-
-    connection.execSql(request);
-
-}
-    function insert_newCustomer(customer_info,done){
+    function insert_newCustomer(connection,customer_info,done){
     var request = new Request("INSERT INTO dbo.Customer (FirstName,LastName,Email,Username,Password,customerType,studentID,professorID,NationalID,CustomerPicture,Reserveable) values (@firstName,@lastName,@email,@username,@password,@occupation,@studentID,@professorID,@CitizenID,@profilePic,@reserveAble)",
     //CustomerPicture,profilePic
         function (err, rowCount, rows){
@@ -172,48 +124,7 @@ app.use(bodyParser.urlencoded({extended: true}));
     })
     connection.execSql(request);
 }
-    function _login(req,username,password,done){
-        console.log('log-in requested');
-        var request = new Request(
-            "SELECT * FROM dbo.customer WHERE Username = @username Or Email = @username",
-            function(err, rowCount, rows){
-                console.log(username);
-                console.log(rowCount);
-                //console.log(rows);
-                console.log(rows);
 
-                console.log('returned rows are above')
-                if(err){
-                    return done(err);
-                }
-                if(rows == null){
-                    console.log('loginMessage', 'No user found.');
-                    return done(null, false);
-                      // req.flash is the way to set flashdata using connect-flash
-                }else if (!(login_request[1] == password)){
-                    console.log('loginMessage', 'Oops! Wrong password.');
-                    return done(null, false);
-                     // create the loginMessage and save it to session as flashdata
-                }else{
-                    console.log('logged in!!!');
-                    return done(null, login_request);
-                }
-        });
-        request.addParameter('username',TYPES.VarChar,username);
-        var login_request = [];
-        request.on('row', function (columns) {
-            columns.forEach(function(column) {
-                login_request.push(column.value);
-            });
-            console.log(login_request + 'info');
-        });
-
-        request.on('Done',function(err, rowCount, rows){
-            console.log(loggedInBoolean());
-        });
-
-        connection.execSql(request);
-    }
     //for login session
     passport.serializeUser(function(user, done) {
         console.log('serializer');
@@ -221,14 +132,42 @@ app.use(bodyParser.urlencoded({extended: true}));
         done(null, user[0]);
     });
     passport.deserializeUser(function(user, done) {
-      console.log('deserializer')
-        connection.on('connect',function(err){
-            if(err){
-                console.log(err);
-            }else{
+        console.log('deserializer')
+        pool.acquire(function (err, connection) {
+            if (err) {
+              console.error(err);
+              return;
             }
-        });
-        deserializer(user,done);
+            var request = new Request(
+                "SELECT * FROM dbo.Customer WHERE Username = @username",
+                function(err,rows){
+                    connection.release();
+                }
+            );
+            //set parameterized query
+            request.addParameter('username',TYPES.VarChar,user[0]);
+            var deserializing = [];
+            request.on('row', function (columns) {
+                columns.forEach(function(column) {
+                    deserializing.push(column.value);
+                });
+                //console.log(deserializing);
+            });
+
+            request.on('requestCompleted', function (err,rows) {
+                // Next SQL statement.
+                done(err, deserializing);
+            });
+
+            connection.execSql(request);
+          });
+        // connection.on('connect',function(err){
+        //     if(err){
+        //         console.log(err);
+        //     }else{
+        //     }
+        // });
+        // deserializer(user,done);
 
     });
 
@@ -241,18 +180,83 @@ app.use(bodyParser.urlencoded({extended: true}));
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req,username,password,done) {
-        console.log('check 01');
+        //console.log('check 01');
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        connection.on('connect',function(err){
-            if(err){
-                console.log(err);
-                //connection.close();
-            }else{
-                console.log('check 02');
+        pool.acquire(function (err, connection) {
+            if (err) {
+              console.error(err);
+              return;
             }
+            console.log('Sign-up requested')
+            //var IMG = base64_encode(req.body.profilePic);
+            var img = fs.readFileSync(req.file.path);
+            var encode_image = img.toString('base64');
+            //console.log(encode_image);
+
+            // var finalImg = {
+            //      contentType: req.file.mimetype,
+            //      image:  new Buffer(encode_image, 'base64')
+            // };
+            var customer_info = {
+              username :req.body.username,
+              password : req.body.password,
+              passwordCheck : req.body.passwordConfirmation,
+              email : req.body.email,
+              fname : req.body.firstName,
+              lname : req.body.lastName,
+              occupation: req.body.occupation,
+              studentID: req.body.studentID,
+              professorID: req.body.professorID,
+              guestID: req.body.NationalID,
+              CustomerPicture: encode_image,
+              Reserveable: 1,
+            };
+            var request = new Request(
+                "SELECT * FROM dbo.Customer WHERE Username = @username",
+                function (err, rowCount, rows){
+                    //console.log(rows);
+                    //console.log("above row object");
+                    if (err){
+                        console.log("signup error");
+                        return done(err);
+                    }
+                    if (customer_info.password!=customer_info.passwordCheck){
+                        console.log('password does not match');
+                        return done(null,false);
+                    }
+                    if (rows.length != 0) {
+                        console.log('this email is already taken');
+                        return done(null, false);
+                    }else {
+
+                        console.log('this email doesnot taken')
+                        // if there is no user with that email
+                        // create the use
+                        var newUserMysql = new Object();
+
+                        newUserMysql.username = username;
+                        newUserMysql.password = password; // use the generateHash function in our user model
+
+                        newUserMysql.id = rows.insertId;
+
+                        insert_newCustomer(connection,customer_info,done)
+                        return done(null, newUserMysql);
+                    }
+                    connection.release();
+                }
+            );
+            //set parameterized query
+            request.addParameter('username',TYPES.VarChar,customer_info.username);
+            request.addParameter('password',TYPES.VarChar,customer_info.password);
+
+            request.on('requestCompleted', function () {
+
+            });
+
+            connection.execSql(request);
         });
-        _signup(req,username,password,done);
+        //_signup(req,username,password,done);
 
         //res.redirect('/login');
     }));
@@ -267,13 +271,63 @@ app.use(bodyParser.urlencoded({extended: true}));
         // callback with email and password from our form
 
         console.log('trying to login');
-        connection.on('connect',function(err){
-            if(err){
-                console.log(err);
-            }else{
+        pool.acquire(function (err, connection) {
+            if (err) {
+              console.error(err);
+              return;
             }
+            console.log('log-in requested');
+            var request = new Request(
+                'SELECT * FROM dbo.Customer WHERE Username = @username Or Email = @username',
+                function(err, rowCount, rows){
+                    console.log(username);
+                    console.log(rowCount);
+
+                    console.log('number of row returned')
+                    if(err){
+                        //connection.release();
+                        return done(err);
+                    }
+                    if(rows == null){
+                        console.log('loginMessage', 'No user found.');
+                        //connection.release();
+                        return done(null, false);
+                          // req.flash is the way to set flashdata using connect-flash
+                    }else if (!(login_request[1] == password)){
+                        console.log('loginMessage', 'Oops! Wrong password.');
+                        //connection.release();
+                        return done(null, false);
+                         // create the loginMessage and save it to session as flashdata
+                    }else{
+                        console.log('logged in!!!');
+                        //connection.release();
+                        return done(null, login_request);
+                    }
+                    connection.release();
+            });
+            request.addParameter('username',TYPES.VarChar,username);
+            var login_request = [];
+            request.on('row', function (columns) {
+                columns.forEach(function(column) {
+                    login_request.push(column.value);
+                });
+                //console.log(login_request + 'info');
+            });
+
+            request.on('Done',function(err, rowCount, rows){
+                console.log(loggedInBoolean());
+            });
+
+            connection.execSql(request);
+            //_login(req, username, password, done, );
         });
-        _login(req, username, password, done);
+
+        // connection.on('connect',function(err){
+        //     if(err){
+        //         console.log(err);
+        //     }else{
+        //     }
+        // });
 
     }));
 
