59d6d0923416ec9582d03f87b371b1c53cc0f894
Theetouch J <theetouchjareonwatanan@Theetouchs-MacBook-Pro.local>
2019-04-23 15:24:39 +0700

    scanner

--- ParkKingDraft4/index0.js
+++ ParkKingDraft4/index0.js
@@ -857,55 +857,36 @@ app.get('/showqr', loggedIn,hasReserved, async function(req, res){
 // import QrReader from 'react-qr-reader'
 
 app.get('/scanner',loggedIn, function(req,res){
-  // global.$ = $;
-  // let Instascan = require('instascan');
-  // console.log('try to scan...');
-  // let scanner = new Instascan.Scanner(
-  //     {
-  //       // Whether to scan continuously for QR codes. If false, use scanner.scan() to manually scan.
-  //       // If true, the scanner emits the "scan" event when a QR code is scanned. Default true.
-  //       continuous: true,
+  // 
+  // class Test extends Component {
+  //   state = {
+  //     result: 'No result'
+  //   }
   //
-  //       // The HTML element to use for the camera's video preview. Must be a <video> element.
-  //       // When the camera is active, this element will have the "active" CSS class, otherwise,
-  //       // it will have the "inactive" class. By default, an invisible element will be created to
-  //       // host the video.
-  //       video: document.getElementById('preview'),
-  //
-  //       // Whether to horizontally mirror the video preview. This is helpful when trying to
-  //       // scan a QR code with a user-facing camera. Default true.
-  //       mirror: true,
-  //
-  //       // Whether to include the scanned image data as part of the scan result. See the "scan" event
-  //       // for image format details. Default false.
-  //       captureImage: false,
-  //
-  //       // Only applies to continuous mode. Whether to actively scan when the tab is not active.
-  //       // When false, this reduces CPU usage when the tab is not active. Default true.
-  //       backgroundScan: true,
-  //
-  //       // Only applies to continuous mode. The period, in milliseconds, before the same QR code
-  //       // will be recognized in succession. Default 5000 (5 seconds).
-  //       refractoryPeriod: 5000,
-  //
-  //       // Only applies to continuous mode. The period, in rendered frames, between scans. A lower scan period
-  //       // increases CPU usage but makes scan response faster. Default 1 (i.e. analyze every frame).
-  //       scanPeriod: 1
+  //   handleScan = data => {
+  //     if (data) {
+  //       this.setState({
+  //         result: data
+  //       })
   //     }
-  // );
-  // scanner.addListener('scan', function(content) {
-  //     alert('Do you want to open this page?: ' + content);
-  //     window.open(content, "_blank");
-  //     //console.log(content);
-  // });
-  // Instascan.Camera.getCameras().then(cameras =>
-  // {
-  //     if(cameras.length > 0){
-  //         scanner.start(cameras[0]);
-  //     } else {
-  //         console.error("Please enable Camera!");
-  //     }
-  // });
+  //   }
+  //   handleError = err => {
+  //     console.error(err)
+  //   }
+  //   render() {
+  //     return (
+  //       <div>
+  //         <QrReader
+  //           delay={300}
+  //           onError={this.handleError}
+  //           onScan={this.handleScan}
+  //           style={{ width: '100%' }}
+  //         />
+  //         <p>{this.state.result}</p>
+  //       </div>
+  //     )
+  //   }
+  // }
   res.render('scanner');
 });
 
