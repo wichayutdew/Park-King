eeb36e1026e6bda6465c4e4acedf65f91d361d18
Nachanon Chanjarasvichai <newzclan@hotmail.com>
2019-04-24 18:39:47 +0700

    send qrcode to node js

--- ParkKingDraft4/index.js
+++ ParkKingDraft4/index.js
@@ -1587,6 +1587,10 @@ app.post('/register', upload.single('profilePic'),passport.authenticate('local-s
     session: false
 }));
 
+app.post('/qrcode', function(req, res){
+  console.log(req.body.data);
+});
+
 app.listen(3000, process.env.IP, function(){
     console.log('Park King Server is running on port 3000.....');
 });
